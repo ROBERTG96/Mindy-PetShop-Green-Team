ea49d7c1333626ef4299a35a749987f3ba7369d3
and1kmpo <acampolpez@gmail.com>
2023-05-03 00:44:20 -0500

    Section medicines & toys

--- js/catalog.js
+++ js/catalog.js
@@ -1,13 +1,16 @@
 const urlApi = "https://pro-talento.up.railway.app/api/mindy/products";
 let Api = new Array();
+let ApiMedicamentos = new Array();
+let ApiJuguetes = new Array();
 
 async function getApi(urlApi) {
     try {
         let response = await fetch(urlApi);
         response = await response.json();
         Api = response.products;
-        console.log(response);
-        getProducts(Api);
+        ApiMedicamentos = response.products.filter(categoria => categoria.tipo === "Medicamento");
+        ApiJuguetes = response.products.filter(categoria => categoria.tipo === "Juguete");
+        getProducts(ApiMedicamentos, ApiJuguetes);
     } catch (error) {
         console.error(error);
     }
@@ -16,43 +19,76 @@ async function getApi(urlApi) {
 getApi(urlApi);
 
 
-const cardsContainer = document.getElementById("cards-container");
+const cardsContainerMedicamentos = document.getElementById("cards-container-medicamentos");
+const cardsContainerJuguetes = document.getElementById("cards-container-juguetes");
 
-function getProducts(Api) {
-    console.log(Api);
-    Api.forEach((Api) => {
-        const card = document.createElement("div");
+function getProducts(medicamentos, juguetes) {
+    console.log(medicamentos);
+    medicamentos.forEach((medicamento) => {
+        let card = document.createElement("div");
         card.className = "col-md-4 mb-4";
         card.innerHTML = `
       <div class="card h-100">
-        <img src="${Api.imagen}" class="card-img-top" alt="${Api.nombre}">
+        <img src="${medicamento.imagen}" class="card-img-top" alt="${medicamento.nombre}">
         <div class="card-body mb-3">
-          <h5 class="card-title mb-3">${Api.nombre}</h5>
+          <h5 class="card-title mb-3">${medicamento.nombre}</h5>
           <div class="row mb-3">
             <div class="col-6">
-              <p class="card-text"><i class="fas fa-dollar-sign"></i> ${Api.precio}</p>
+              <p class="card-text"><i class="fas fa-dollar-sign"></i> ${medicamento.precio}</p>
             </div>
             <div class="col-6">
-            <p class="card-text"><i class="fas fa-tags"></i> ${Api.tipo}</p>
+            <p class="card-text"><i class="fas fa-tags"></i> ${medicamento.tipo}</p>
           </div>
           </div>
-          <p class="card-text mb-3">${Api.descripcion}</p>
+          <p class="card-text mb-3">${medicamento.descripcion}</p>
           <div class="input-group mb-3">
             <button class="btn btn-outline-secondary" type="button" id="button-minus-0">-</button>
             <input type="text" class="form-control" placeholder="Cantidad" aria-label="Cantidad" value="0" id="quantity-0">
             <button class="btn btn-outline-secondary" type="button" id="button-plus-0">+</button>
-            <span class="input-group-text">Existencias: <span id="stock-0">${Api.stock}</span></span>
+            <span class="input-group-text">Existencias: <span id="stock-0">${medicamento.stock}</span></span>
           </div>
           <div class="d-flex justify-content-center">
-            <button type="button" class="btn btn-primary position-absolute bottom-0 mb-2" onclick="agregarAlCarrito(${JSON.stringify(Api)})">
+            <button type="button" class="btn btn-primary position-absolute bottom-0 mb-2" onclick="agregarAlCarrito(${JSON.stringify(medicamento)})">
               <i class="fas fa-shopping-cart"></i> Agregar al carrito
             </button>
           </div>
         </div>
       </div>
     `;
-        cardsContainer.appendChild(card);
+        cardsContainerMedicamentos.appendChild(card);
+    });
+
+    ApiJuguetes.forEach((Juguetes) => {
+        const card = document.createElement("div");
+        card.className = "col-md-4 mb-4";
+        card.innerHTML = `
+    <div class="card h-100">
+      <img src="${Juguetes.imagen}" class="card-img-top" alt="${Juguetes.nombre}">
+      <div class="card-body mb-3">
+        <h5 class="card-title mb-3">${Juguetes.nombre}</h5>
+        <div class="row mb-3">
+          <div class="col-6">
+            <p class="card-text"><i class="fas fa-dollar-sign"></i> ${Juguetes.precio}</p>
+          </div>
+          <div class="col-6">
+          <p class="card-text"><i class="fas fa-tags"></i> ${Juguetes.tipo}</p>
+        </div>
+        </div>
+        <p class="card-text mb-3">${Juguetes.descripcion}</p>
+        <div class="input-group mb-3">
+          <button class="btn btn-outline-secondary" type="button" id="button-minus-0">-</button>
+          <input type="text" class="form-control" placeholder="Cantidad" aria-label="Cantidad" value="0" id="quantity-0">
+          <button class="btn btn-outline-secondary" type="button" id="button-plus-0">+</button>
+          <span class="input-group-text">Existencias: <span id="stock-0">${Juguetes.stock}</span></span>
+        </div>
+        <div class="d-flex justify-content-center">
+          <button type="button" class="btn btn-primary position-absolute bottom-0 mb-2" onclick="agregarAlCarrito(${JSON.stringify(Juguetes)})">
+            <i class="fas fa-shopping-cart"></i> Agregar al carrito
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
+        cardsContainerJuguetes.appendChild(card);
     });
 }
-
-getProducts(Api);
