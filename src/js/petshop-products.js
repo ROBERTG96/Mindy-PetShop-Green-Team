1092b21529e797b533cfb3df3b00c2328ba049d9
and1kmpo <acampolpez@gmail.com>
2023-05-06 03:10:30 -0500

    Add items cart from HTML

--- src/js/petshop-products.js
+++ src/js/petshop-products.js
@@ -5,23 +5,23 @@ let ApiJuguetes = new Array();
 
 
 async function ApiFetch() {
-  let response = await fetch(urlApi);
-  response = await response.json();
-  return response;
+    let response = await fetch(urlApi);
+    response = await response.json();
+    return response;
 }
 
 async function getApi(urlApi) {
-  try {
-    let response = await fetch(urlApi);
-    response = await response.json();
-    Api = response.products;
-    ApiMedicamentos = response.products.filter(categoria => categoria.tipo === "Medicamento");
-    ApiJuguetes = response.products.filter(categoria => categoria.tipo === "Juguete");
-    getProductsMedicamentos(ApiMedicamentos);
-    getProductsJuguetes(ApiJuguetes);
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        let response = await fetch(urlApi);
+        response = await response.json();
+        Api = response.products;
+        ApiMedicamentos = response.products.filter(categoria => categoria.tipo === "Medicamento");
+        ApiJuguetes = response.products.filter(categoria => categoria.tipo === "Juguete");
+        getProductsMedicamentos(ApiMedicamentos);
+        getProductsJuguetes(ApiJuguetes);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 getApi(urlApi);
@@ -30,10 +30,10 @@ const cardsContainerMedicamentos = document.getElementById("cards-container-medi
 const cardsContainerJuguetes = document.getElementById("cards-container-juguetes");
 
 function getProductsMedicamentos(Medicamentos) {
-  Medicamentos.forEach((medicamento) => {
-    let card = document.createElement("div");
-    card.className = "col-md-4 mb-4";
-    card.innerHTML = `
+    Medicamentos.forEach((medicamento) => {
+        let card = document.createElement("div");
+        card.className = "col-md-4 mb-4";
+        card.innerHTML = `
       <div class="card rounded shadow h-100">
         <img src="${medicamento.imagen}" class="card-img-top" alt="${medicamento.nombre}">
         <div class="card-body color_primary text-light mb-3">
@@ -59,21 +59,21 @@ function getProductsMedicamentos(Medicamentos) {
        </div>
 
         <div class="d-flex justify-content-center mb-1">
-            <button type="button" class="btn btn-block btn-light" onclick="agregarAlCarrito(${JSON.stringify(medicamento)})">
+            <a class="btn btn-block btn-light" onclick="agregarAlCarrito('${medicamento._id}')">
               <i class="fas fa-shopping-cart btn-icon"></i> Agregar al carrito
-            </button>
+            </a>
           </div>
       </div>
     `;
-    cardsContainerMedicamentos.appendChild(card);
-  });
+        cardsContainerMedicamentos.appendChild(card);
+    });
 }
 
 function getProductsJuguetes(Juguetes) {
-  Juguetes.forEach((Juguete) => {
-    const card = document.createElement("div");
-    card.className = "col-md-4 mb-4";
-    card.innerHTML = `
+    Juguetes.forEach((Juguete) => {
+        const card = document.createElement("div");
+        card.className = "col-md-4 mb-4";
+        card.innerHTML = `
       <div class="card rounded shadow h-100">
         <img src="${Juguete.imagen}" class="card-img-top" alt="${Juguete.nombre}">
         <div class="card-body color_primary text-light mb-3">
@@ -99,112 +99,148 @@ function getProductsJuguetes(Juguetes) {
        </div>
 
         <div class="d-flex justify-content-center mb-1">
-            <button type="button" class="btn btn-block btn-light" onclick="agregarAlCarrito(${JSON.stringify(Juguete)})">
+            <a class="btn btn-block btn-light" onclick="agregarAlCarrito('${Juguete._id}')">
               <i class="fas fa-shopping-cart btn-icon"></i> Agregar al carrito
-            </button>
+            </a>
           </div>
       </div>
     `;
-    cardsContainerJuguetes.appendChild(card);
-  });
+        cardsContainerJuguetes.appendChild(card);
+    });
 }
 
-function resetearTemplateMedicamentos() {
-  cardsContainerMedicamentos.innerHTML = '';
+
+let carrito = [];
+
+function agregarAlCarrito(idProducto) {
+    const item = Api.find(producto => producto._id === idProducto);
+    carrito.push(item);
+    mostrarCarrito();
 }
+
+const mostrarCarrito = () => {
+    const modalBody = document.querySelector('.modal .modal-body');
+    modalBody.innerHTML = '';
+
+    carrito.forEach((producto) => {
+        modalBody.innerHTML += `<div class="modal-contenedor">
+        <hr>
+        <div>
+            <img src="${producto.imagen}" class="img-fluid img-carrito" alt="">
+        </div>
+    
+        <div>
+            <p>Producto: ${producto.nombre}</p>
+            <p>Precio: ${producto.precio}</p>
+            <p>Cantidad: ${producto.stock}</p>
+    
+            <button class="btn btn-danger mb-4">Eliminar del carrito</button>
+        </div>
+        
+    </div>`
+    });
+}
+
+
+
+
+function resetearTemplateMedicamentos() {
+    cardsContainerMedicamentos.innerHTML = '';
+}
+
 function resetearTemplateJuguetes() {
-  cardsContainerJuguetes.innerHTML = '';
+    cardsContainerJuguetes.innerHTML = '';
 }
 
 async function filtrar_medicamentos() {
 
-  let InputSearchMedicamento = document.querySelector('#BuscarMedicamento');
-  let BusquedaMedicamento = InputSearchMedicamento.value.toLowerCase();
+    let InputSearchMedicamento = document.querySelector('#BuscarMedicamento');
+    let BusquedaMedicamento = InputSearchMedicamento.value.toLowerCase();
 
-  let api = await ApiFetch();
-  let medicamentos = api.products.filter(categoria => categoria.tipo === "Medicamento");
+    let api = await ApiFetch();
+    let medicamentos = api.products.filter(categoria => categoria.tipo === "Medicamento");
 
-  let MedicamentosFiltrados = medicamentos.filter(medicamento => {
-    medicamento.nombre = medicamento.nombre.toLowerCase();
-    medicamento.descripcion = medicamento.descripcion.toLowerCase();
+    let MedicamentosFiltrados = medicamentos.filter(medicamento => {
+        medicamento.nombre = medicamento.nombre.toLowerCase();
+        medicamento.descripcion = medicamento.descripcion.toLowerCase();
 
-    return medicamento.nombre.indexOf(BusquedaMedicamento) > - 1 || medicamento.descripcion.indexOf(BusquedaMedicamento) > -1;
-  })
+        return medicamento.nombre.indexOf(BusquedaMedicamento) > -1 || medicamento.descripcion.indexOf(BusquedaMedicamento) > -1;
+    })
 
-  console.log('MedicamentosFiltrados:', MedicamentosFiltrados);
+    console.log('MedicamentosFiltrados:', MedicamentosFiltrados);
 
-  if (MedicamentosFiltrados.length === 0) {
-    resetearTemplateMedicamentos();
-    busquedaMedicamentoNoEncontrada();
-  } else {
-    resetearTemplateMedicamentos();
-    getProductsMedicamentos(MedicamentosFiltrados)
-  }
+    if (MedicamentosFiltrados.length === 0) {
+        resetearTemplateMedicamentos();
+        busquedaMedicamentoNoEncontrada();
+    } else {
+        resetearTemplateMedicamentos();
+        getProductsMedicamentos(MedicamentosFiltrados)
+    }
 
 }
 
 async function filtrarPrecios() {
 
-  let minimo = Number(document.querySelector('#precioMinimo').value);
-  let maximo = Number(document.querySelector('#precioMaximo').value);
-  let desc = document.querySelector('#desc_med');
+    let minimo = Number(document.querySelector('#precioMinimo').value);
+    let maximo = Number(document.querySelector('#precioMaximo').value);
+    let desc = document.querySelector('#desc_med');
 
-  if (!minimo) {
-    Swal.fire({
-      icon: 'info',
-      title: '¡Debes ingresar un precio mínimo!',
-      text: 'Para filtrar por precio ambos campos deben contener valores mayor a 0.'
-    })
-  } else if (!maximo) {
-    Swal.fire({
-      icon: 'info',
-      title: '¡Debes ingresar un precio máximo!',
-      text: 'Para filtrar por precio ambos campos deben contener valores mayor a 0.'
-    })
-  } else if (minimo && maximo) {
+    if (!minimo) {
+        Swal.fire({
+            icon: 'info',
+            title: '¡Debes ingresar un precio mínimo!',
+            text: 'Para filtrar por precio ambos campos deben contener valores mayor a 0.'
+        })
+    } else if (!maximo) {
+        Swal.fire({
+            icon: 'info',
+            title: '¡Debes ingresar un precio máximo!',
+            text: 'Para filtrar por precio ambos campos deben contener valores mayor a 0.'
+        })
+    } else if (minimo && maximo) {
 
-    let InputSearchMedicamento = document.querySelector('#BuscarMedicamento');
-    let BusquedaMedicamento = InputSearchMedicamento.value.toLowerCase();
-    let medicamentos;
+        let InputSearchMedicamento = document.querySelector('#BuscarMedicamento');
+        let BusquedaMedicamento = InputSearchMedicamento.value.toLowerCase();
+        let medicamentos;
 
-    if (desc.checked) {
-      medicamentos = await fetch(`${urlApi}?tipo=medicamento&orden=desc`);
-      medicamentos = await medicamentos.json();
+        if (desc.checked) {
+            medicamentos = await fetch(`${urlApi}?tipo=medicamento&orden=desc`);
+            medicamentos = await medicamentos.json();
 
-    } else {
-      medicamentos = await fetch(`${urlApi}?tipo=medicamento&orden=asc`);
-      medicamentos = await medicamentos.json();
+        } else {
+            medicamentos = await fetch(`${urlApi}?tipo=medicamento&orden=asc`);
+            medicamentos = await medicamentos.json();
+        }
+
+        let MedicamentosFiltrados = medicamentos.products.filter(medicamento => {
+            medicamento.nombre = medicamento.nombre.toLowerCase();
+            medicamento.descripcion = medicamento.descripcion.toLowerCase();
+
+            return (medicamento.nombre.indexOf(BusquedaMedicamento) > -1 || medicamento.descripcion.indexOf(BusquedaMedicamento) > -1) && (medicamento.precio >= minimo && medicamento.precio <= maximo);
+        })
+
+        if (MedicamentosFiltrados.length === 0) {
+            resetearTemplateMedicamentos();
+            busquedaMedicamentoNoEncontrada();
+        } else {
+            resetearTemplateMedicamentos();
+            getProductsMedicamentos(MedicamentosFiltrados)
+        }
     }
-
-    let MedicamentosFiltrados = medicamentos.products?.filter(medicamento => {
-      medicamento.nombre = medicamento.nombre.toLowerCase();
-      medicamento.descripcion = medicamento.descripcion.toLowerCase();
-
-      return (medicamento.nombre.indexOf(BusquedaMedicamento) > - 1 || medicamento.descripcion.indexOf(BusquedaMedicamento) > -1) && (medicamento.precio >= minimo && medicamento.precio <= maximo);
-    })
-
-    if (MedicamentosFiltrados.length === 0) {
-      resetearTemplateMedicamentos();
-      busquedaMedicamentoNoEncontrada();
-    } else {
-      resetearTemplateMedicamentos();
-      getProductsMedicamentos(MedicamentosFiltrados)
-    }
-  }
 }
 
 async function ordenarPorPrecio(asc, des) {
-  let api = await ApiFetch();
-  let medicamentos = api.products.filter(categoria => categoria.tipo === "Medicamento");
+    let api = await ApiFetch();
+    let medicamentos = api.products.filter(categoria => categoria.tipo === "Medicamento");
 
-  console.log('ordenando asc:', asc);
-  console.log(medicamentos);
+    console.log('ordenando asc:', asc);
+    console.log(medicamentos);
 }
 
 
 function busquedaMedicamentoNoEncontrada() {
-  cardsContainerMedicamentos.innerHTML = ''
-  cardsContainerMedicamentos.innerHTML = `<div class="d-flex shadow align-items-center justify-content-center ">
+    cardsContainerMedicamentos.innerHTML = ''
+    cardsContainerMedicamentos.innerHTML = `<div class="d-flex shadow align-items-center justify-content-center ">
   <div class="text-center mb-3">
       <h5 class="display-1 text-danger">Productos no encontrados</h5>
       <p class="fs-3 text-dark"> <span class="text-danger">¡</span> La busqueda no funcionó <span class="text-danger">!</span></p>
