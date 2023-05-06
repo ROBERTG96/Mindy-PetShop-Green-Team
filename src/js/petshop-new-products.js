421bb0e09fbada8f7b7f60c84d0e2756786bb68e
ROBERTG96 <71914244+ROBERTG96@users.noreply.github.com>
2023-05-05 21:43:07 -0500

    Modificación formulario

--- src/js/petshop-new-products.js
+++ src/js/petshop-new-products.js
@@ -1,64 +1,65 @@
 const urlApi = "https://pro-talento.up.railway.app/api/mindy/products";
 let data = new FormData();
 
-async function NuevoProducto() {
+let formulario = document.getElementById('FormRegistroProducto');
 
-    let nombre = document.getElementsByName('nombre')[0].value;
-    let descripcion = document.getElementsByName('descripcion')[0].value;
-    let imagen = document.getElementsByName('imagen')[0].value;
-    let tipo = document.getElementsByName('tipo')[0].value;
-    let precio = document.getElementsByName('precio')[0].value;
-    let stock = document.getElementsByName('stock')[0].value;
+formulario.addEventListener('submit', async e => {
+    e.preventDefault();
+    try {
 
-    data = {
-        nombre,
-        descripcion,
-        imagen,
-        tipo,
-        precio,
-        stock
-    }
+        for (let input of formulario) {
+            if (input.name) {
+                data[input.name] = input.value
+            }
+        }
 
-    await fetch(urlApi, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-        }, body: JSON.stringify(data)
-    })
-        .then(async res => {
-            if (res.status <= 201) {
+        await fetch(urlApi, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }, body: JSON.stringify(data)
+        })
+            .then(async res => {
+                if (res.status <= 201) {
 
-                document.getElementById("FormRegistroProducto").reset();
-                document.getElementsByName('nombre')[0].focus();
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Producto registrado con éxito',
-                    text: 'Nuevo producto agregado al stock.',
-                    timer: 3000
-                })
-                return await res.json();
+                    document.getElementById("FormRegistroProducto").reset();
+                    document.getElementsByName('nombre')[0].focus();
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Producto registrado con éxito',
+                        text: 'Nuevo producto agregado al stock.',
+                        timer: 2000
+                    })
+                    return await res.json();
 
-            } else {
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Ocurrió un error en el proceso de registro.',
+                        text: 'Verifica que los campos no estėn vacíos.',
+                        timer: 3000
+                    })
+                }
+            })
+            .catch(err => {
                 Swal.fire({
                     icon: 'error',
                     title: 'Ocurrió un error en el proceso de registro.',
-                    text: 'Verifica que los campos no estėn vacíos.',
+                    text: err,
                     timer: 3000
                 })
-            }
+                console.log(err);
+            });
+    } catch (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Ocurrió un error en el proceso de registro.',
+            text: err,
+            timer: 3000
         })
-        .catch(err => {
-            Swal.fire({
-                icon: 'error',
-                title: 'Ocurrió un error en el proceso de registro.',
-                text: err,
-                timer: 3000
-            })
-            console.log(err);
-        });
-
-}
+        console.log(error);
+    }
+})
 
 
 function LimpiarFormulario() {
